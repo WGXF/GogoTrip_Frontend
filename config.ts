100fa51cfb89b12a4b4e242f107cec06f4a9ca13
Toh Yuan Kai <72385447+WGXF@users.noreply.github.com>
2025-12-11 14:03:14 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,8 @@
 // 集中管理配置的文件
 
 // 从环境变量读取，如果没有设置，则使用默认值作为后备
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 // 你也可以在这里放其他的全局配置
+
 export const APP_NAME = "GogoTrip";
